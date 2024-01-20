0e7e0a802c18659ec43cc49f81f57344b461700e
Khanhtoan220901 <110116190+Khanhtoan220901@users.noreply.github.com>
2024-01-20 23:23:39 +0700

    app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from st_pages import Page, Section, show_pages, add_page_title
 
 show_pages(
-    [   Page(r"C:/Users/khanh/Documents/Market Analyst/Home.py", "Home", "🏠"),
-        Page(r"C:/Users/khanh/Documents/Market Analyst/Sales_performance.py", "Top Sales Corporation", "📊"),
+    [   Page("Home.py", "Home", "🏠"),
+        Page("Sales_performance.py", "Top Sales Corporation", "📊"),
         Section(name="Benchmark company performance vs. market", icon="📈"),
-          Page(r"C:/Users/khanh/Documents/Market Analyst/Overview.py", "Overview"),
-          Page(r"C:/Users/khanh/Documents/Market Analyst/Mocules.py", "Mocules"),
-          Page(r"C:/Users/khanh/Documents/Market Analyst/Content.py", "Content"),
-          Page(r"C:/Users/khanh/Documents/Market Analyst/Channel.py", "Channel"),
-          Page(r"C:/Users/khanh/Documents/Market Analyst/Branded_GX.py", "Branded_GX"),
+          Page("Overview.py", "Overview"),
+          Page("Mocules.py", "Mocules"),
+          Page("Content.py", "Content"),
+          Page("Channel.py", "Channel"),
+          Page("Branded_GX.py", "Branded_GX"),
 
     ]
 )
